656901f175a028e18037976c94a439d4f1b905bd
Seth Reece <sethorama@bassplayer.st>
2015-10-21 10:13:40 -0700

    Cloned

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php 
+$pageTitle = "Company Info";
+$section = "info";
+include('inc/header.php'); ?>
+
+	<div class="section page">
+		<h1>About Us</h1>
+	</div>
+
+<?php include('inc/footer.php'); ?>
---
+++ receipt.php
@@ -0,0 +1,16 @@
+<?php
+$pageTitle = "Thank you for your order!";
+$section = "none";
+include("inc/header.php"); ?>
+
+  <div class ="section page">
+
+    <div class="wrapper">
+
+      <h1>Thank You!</h1>
+      <p>Thank you for your payment. Your transaction has been completed, and a receipt for your purchase has been emailed to you. You may log into your account at <a href="http://www.paypal.com/us">www.paypal.com/us</a> to view details of this transaction.</p>
+    </div>
+      <p>Need another shirt already? Visit the <a href="shirts.php">shirts page.</a></p>
+  </div>
+
+<?php include("inc/footer.php"); ?>
